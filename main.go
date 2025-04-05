d269e1adc59d69029b6f6a007eafecbe4d9a4875
Dmitriy Panteleev <pant-dm@yandex.ru>
2025-04-05 16:58:59 +0300

    Refactor timestamp parsing to support multiple formats

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 	"time"
 )
 
+// Список поддерживаемых форматов таймштампов
+var timestampFormats = []string{
+	"2006/01/02 15:04:05.000000",    // news-notifier.log и udf.log
+	"2006-01-02 15:04:05,000000000", // papertrading.log
+	// Здесь можно добавить другие форматы при необходимости
+}
+
+func parseTimestamp(timestampStr string) (time.Time, error) {
+	var t time.Time
+	var err error
+	for _, format := range timestampFormats {
+		t, err = time.Parse(format, timestampStr)
+		if err == nil {
+			return t, nil
+		}
+	}
+	return t, fmt.Errorf("неизвестный формат таймштампа: %s", timestampStr)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Использование: go run log_histogram.go <имя_лог_файла>")
@@ -34,9 +53,9 @@ func main() {
 		}
 
 		timestampStr := fields[0] + " " + fields[1]
-		timestamp, err := time.Parse("2006/01/02 15:04:05.000000", timestampStr)
+		timestamp, err := parseTimestamp(timestampStr)
 		if err != nil {
-			fmt.Println("Ошибка парсинга времени:", err)
+			// Если не удалось распарсить таймштамп, пропускаем строку
 			continue
 		}
 
